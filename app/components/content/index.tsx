0a6163db4d784501ae9f04136137e7fc022491eb
AbdelghfarKhairallah1 <140082083+Abdelghfarsalah@users.noreply.github.com>
2026-04-22 17:36:54 +0200

    add change readme file

--- app/components/content/index.tsx
+++ app/components/content/index.tsx
@@ -1,9 +1,12 @@
+import SliderContext from "@/app/context/slidercontext";
+import { useContext } from "react";
 import { MdArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "../style.css";
 export default function Content() {
     const index=useSelector((state:{changeBackground: {index: number}})=>state.changeBackground.index)
-
+    const images=useContext(SliderContext);
+    const dispatch=useDispatch();
     return (<div className="content">
     <div className="pageContent">
         <p>D E S I G N</p>
@@ -18,9 +21,15 @@ export default function Content() {
         }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis eum amet nostrum voluptatem obcaecati labore qui laborum consectetur corporis enim recusandae, suscipit distinctio culpa fugiat, vel nihil unde fugit et.</p>
     </div>
     <div className="sliderbu">
-        <div>
+        <div onClick={()=>{
+            const newIndex=index-1<0?images.length-1:index-1;
+            dispatch({type:"changeBackground/changeBackground",payload:{background:images[newIndex],index:newIndex}})
+        }}>
         <MdArrowBackIosNew /></div>
-        <div>
+        <div onClick={()=>{
+            const newIndex=index+1>=images.length?0:index+1;
+            dispatch({type:"changeBackground/changeBackground",payload:{background:images[newIndex],index:newIndex}})
+        }}>
         <MdArrowForwardIos /></div>
     </div>
     </div>)
